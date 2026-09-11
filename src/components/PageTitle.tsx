ee675c2b1c73d86cd6375ae8b004b87cbc5a8142
Nikolaj <nikolaj.schlumbohm@gmail.com>
2026-09-11 23:15:58 +0200

    Update PageTitle.tsx

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -50,8 +50,8 @@ PageTitle.css = `
 }
 
 .page-title-logo {
-  max-width: 2.5rem;
-  max-height: 2.5rem;
+  max-width: 10rem;
+  max-height: 10rem;
   height: auto;
   width: auto;
   object-fit: contain;
